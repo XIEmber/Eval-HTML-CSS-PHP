3452b378e6506241e29c713dfb215fb37d4ffc8a
Dylan KADANI <dylan.bassier@gmail.com>
2019-12-14 22:43:55 +0100

    CSS (pas encore fini)

--- includes/CV.inc.php
+++ includes/CV.inc.php
@@ -3,6 +3,13 @@
 <p>Liste non-exhaustive de démons sympa à connaître :</p>
 </div>
 
+<div id="Lilith">
+<h1>Lilith</h1>
+<p>Selon la légende, Lilith était la première femme d’Adam. </p>
+<p>Bannie du jardin d’Éden pour son refus de se soumettre, </p>
+<p>elle est décrite comme une démone qui vole des bébés.</p>
+</div>
+
 <div id="Succube">
 <h1>Succube</h1>
 <p>Démon qui revêt une apparence</p>
@@ -13,3 +20,7 @@
 <h1>Incube</h1>
 <p>Les incubes sont des démons et cherchent</p>
 <p>à jouir des femmes quand elles rêvent ou somnolent.</p>
+</div>
+<div id="download">
+<a href="includes\" download>Télécharger le codex</a>
+</div>
