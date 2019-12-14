a579d4924cc0f3e7c6368691825f08fb2af299d7
Dylan KADANI <dylan.bassier@gmail.com>
2019-12-14 23:04:23 +0100

    css + pdf

--- includes/CV.inc.php
+++ includes/CV.inc.php
@@ -4,23 +4,23 @@
 </div>
 
 <div id="Lilith">
-<h1>Lilith</h1>
+<a href="https://fr.wikipedia.org/wiki/Lilith" target="_blank"><h1>Lilith</h1></a>
 <p>Selon la légende, Lilith était la première femme d’Adam. </p>
 <p>Bannie du jardin d’Éden pour son refus de se soumettre, </p>
 <p>elle est décrite comme une démone qui vole des bébés.</p>
 </div>
 
 <div id="Succube">
-<h1>Succube</h1>
+<a href="https://fr.wikipedia.org/wiki/Succube" target="_blank"><h1>Succube</h1></a>
 <p>Démon qui revêt une apparence</p>
 <p>d'une femme pour tenter un homme.</p>
 </div>
 
 <div id="Incube">
-<h1>Incube</h1>
+<a href="https://fr.wikipedia.org/wiki/Incube" target="_blank"><h1>Incube</h1></a>
 <p>Les incubes sont des démons et cherchent</p>
 <p>à jouir des femmes quand elles rêvent ou somnolent.</p>
 </div>
 <div id="download">
-<a href="includes\" download>Télécharger le codex</a>
+<a href="./pdf/démonologie.pdf" download>Télécharger le codex</a>
 </div>
