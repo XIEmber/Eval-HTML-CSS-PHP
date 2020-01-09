30a712df2ab26bbaa28a6da9bc5ef2787a67d96c
Dylan KADANI <dylan.bassier@gmail.com>
2020-01-09 19:28:56 +0100

    MY GOD JAI FAILLI PERDRE TOUT CA

---
+++ includes/CV.inc.php
@@ -0,0 +1,50 @@
+<h1 id="titrecv">Mazikeen Smith , + de 1000 ans et plus encore</h1>
+<img id="photocv"src="./assets/images/mazikeen.png" alt="photo mazikeen"/>
+<div id="mazikeenintro">
+<h1>Son Histoire</h1>
+<p>Mazikeen, également connu sous le surnom de Maze, est un Démon qui a la forme d'une jeune femme. Elle est la meilleure amie de Lucifer et travaillait autrefois à ses côtés à la discothèque Lux de Los Angeles avant de commencer une nouvelle carrière de chasseuse de primes au Département de Police. </p>
+<h1>Histoire ancienne</h1>
+<p>Maze est un démon et existe depuis des milliers d'années. Tandis que Lucifer était le dirigeant de l'Enfer, elle le servait et torturait les âmes des Humains décédés. Lucifer l'a appelée «la plus habile des bourreaux de l'enfer» et «la plus brutale des bourreaux de l'enfer». </p>
+</div>
+<div id="mazikeenbody">
+<h1>Son Parcours Proffesionnelle</h1>
+<h2>Chasseuse de Primes - Los Angeles Police</h2>
+<ul>
+  <li>-Pourchasse des criminels</li>
+  <li>-Sait se battre</li>
+  <li>-Grogne quand il le faut</li>
+</ul>
+<h2>Barman - Los Angeles Lux</h2>
+<ul>
+  <li>-Tient bien l'alcool</li>
+  <li>-Sait se battre</li>
+</ul>
+<h2>Tortionnaire d'âmes - Enfer</h2>
+<ul>
+  <li>-Fouette , Lance des dagues</li>
+  <li>-La luxure c'est son truc</li>
+  <li>-A fouetter Michel en Enfer</li>
+</ul>
+</div>
+<div id="mazikeenpowers">
+  <h1>Ses pouvoirs</h1>
+  <ul>
+    <li>Immortalité : En tant que démon, Mazikeen ne peut pas être tué par des moyens ordinaires. Elle ne vieillit pas et a vécu pendant des millénaires ou peut-être plus longtemps. Cependant, les démons n'ont pas d'âme, donc si elle meurt, elle serait éradiquée de l'existence. </li>
+    <li>Force surhumaine : Mazikeen possède une force supérieure à l'homme; elle pouvait facilement soulever un homme adulte et le pousser dans un mur. Cependant, sa force est inférieure à celle des anges. </li>
+    <li>Vitesse surhumaine : Mazikeen est capable de se déplacer beaucoup plus vite qu'un être humain, elle a été capable de passer d'une chaise à River qui était de l'autre côté de la pièce instantanément. </li>
+  </ul>
+  <h1>Ses habilités</h1>
+  <ul>
+    <li>Combattant expert corps à corps : Mazikeen a montré une grande compétence de combat non armée. Pendant qu'elle était vaincue, elle était capable de combattre Uriel sur un pied d'égalité pendant un certain temps. </li>
+    <li>Experte en bataille avec des bâtons : Pendant un combat dans un bar, Maze a brisé un bâton de piscine en bois en deux morceaux et les a facilement utilisés pour vaincre un groupe d'hommes saouls. </li>
+    <li>Combattant de couteau expert : Mazikeen a également montré une grande habileté au combat avec des couteaux démoniaques quand elle a combattu Amenadiel, bien qu'elle fût finalement incapable de vaincre l'ange. Elle était aussi capable de tenir tête à Uriel pendant un certain temps avant d'être privée de ses couteaux. </li>
+    <li>Persuasion : Dans une certaine mesure, Mazikeen peut convaincre les gens de prendre une décision qu'ils pensent être la leur. </li>
+  </ul>
+  <h1>Ses Hobbies</h1>
+  <ul>
+    <li>-Le Sexe</li>
+    <li>-L'alcool</li>
+    <li>-La torture d'âmes</li>
+    <li>-Bichonner ses dagues</li>
+  </ul>
+</div>
--- includes/header.php
+++ includes/header.php
@@ -6,5 +6,6 @@
     <a href="index.php?page=accueil" title="Retour à la page d'accueil">L'Enfer</a></li>
     <a href="index.php?page=codex" title="Info sur les démons">Codex Des Démons</a></li>
     <a href="index.php?page=contact" title="Pour me conctacter">Parle en privée avec Lucifer </a></li>
+    <a href="index.php?page=CV"title="CV de Mazikeen">CV d'une Démon</a>
   </ul>
 </nav>
